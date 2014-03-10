2a5586697ab95d7cb12bb80247e26001dc491eea
Mario Giovacchini <mario.giovacchini@scilifelab.se>
2014-03-10 12:54:53 +0100

    Automatically determine if reads are paired-end, refactor code, fix up formatting

--- process_radtags_directory.py
+++ process_radtags_directory.py
@@ -13,55 +13,57 @@ import re
 import subprocess
 import sys
 
-def main(rest_enzyme_list = None, input_dir = os.getcwd(), file_pattern='*_1.fastq*', output_dir='process_radtags_output', paired_end=False, overwrite_output_dir=False):
-    enzyme_choices = ('apeKI', 'bamHI', 'dpnII', 'eaeI', 'ecoRI', 'ecoT22I', 'hindIII', 'mluCI', 'mseI', 'mspI', 'ndeI',
-                      'nheI', 'nlaIII', 'notI', 'nsiI', 'pstI', 'sau3AI', 'sbfI', 'sexAI', 'sgrAI', 'sphI', 'taqI', 'xbaI')
+def main(rest_enzyme_list = None, input_dir = os.getcwd(), file_pattern='*_1.fastq*',
+         output_dir='process_radtags_output', overwrite_output_dir=False):
+    enzyme_choices = ('apeKI', 'bamHI', 'dpnII', 'eaeI', 'ecoRI', 'ecoT22I',
+                      'hindIII', 'mluCI', 'mseI', 'mspI', 'ndeI', 'nheI',
+                      'nlaIII', 'notI', 'nsiI', 'pstI', 'sau3AI', 'sbfI',
+                      'sexAI', 'sgrAI', 'sphI', 'taqI', 'xbaI')
     enzyme_choices_lower = map(str.lower, enzyme_choices)
     if not rest_enzyme_list:
         raise ValueError("No restriction enzyme specified; this is a required value.")
     if len(rest_enzyme_list) > 2:
-        print("Too many enzymes specified ({0}). Truncating to first two enzymes (\"{1}\" and \"{2}\").".format(
-               len(rest_enzyme_list), rest_enzyme_list[0], rest_enzyme_list[1]), file=sys.stderr)
+        print("Too many enzymes specified ({0}). Truncating to first two enzymes " \
+              "(\"{1}\" and \"{2}\").".format(len(rest_enzyme_list), rest_enzyme_list[0],
+                                              rest_enzyme_list[1]), file=sys.stderr)
         rest_enzmye_list = rest_enzyme_list[:2]
     for rest_enzyme in rest_enzyme_list:
         if rest_enzyme.lower() not in enzyme_choices_lower:
-            raise ValueError("Restriction enzyme \"{0}\" invalid. Valid values include:\n{1}".format(rest_enzyme, ", ".join(enzyme_choices)))
+            raise ValueError("Restriction enzyme \"{0}\" invalid. Valid values " \
+                             "include:\n{1}".format(rest_enzyme, ", ".join(enzyme_choices)))
     files_to_process = []
     input_dir = os.path.abspath(input_dir)
-    ## TODO this is a hack but fix this later so you don't have to write the same function twice in a row
-    if paired_end:
-        # Walk the directory, finding pairs of fastq files
-        ## TODO add non-recursive method that doesn't walk the directory
-        for root, dirs, files in os.walk(input_dir):
-            read_1_files = fnmatch.filter(files, file_pattern)
-            for filename in read_1_files:
-                m = re.match('(?P<file_name>.*)_1.fastq(?P<gzip_ext>.gz\w{0,2})', filename)
-                if m:
-                    read_1 = filename
-                    read_2_string = "{0}_2.fastq{1}".format(m.group('file_name'), m.group('gzip_ext'))
-                    read_2 = files.pop(files.index(read_2_string))
-                    files_to_process.append({ "files"       : [ os.path.abspath(os.path.join(root,read_1)),
-                                                                os.path.abspath(os.path.join(root,read_2))],
-                                              "gzip"        :   m.group('gzip_ext'),
-                                              "merge_file"  : "{0}.fastq".format(m.group('file_name')),})
-    else:
-        for root, dirs, files in os.walk(input_dir):
-            matching_files = fnmatch.filter(files, file_pattern)
-            for filename in matching_files:
-                m = re.match('(?P<file_name>.*).fastq(?P<gzip_ext>.gz)', filename)
-                files_to_process.append({   "files"         : [ os.path.abspath(os.path.join(root,filename))],
-                                            "gzip"          :   m.group('gzip_ext'),
-                                            "merge_file"    :   None})
+    naming_pattern = re.compile('(?P<file_name>.+?)(?P<read_number>_1){0,1}.fastq(?P<gzip_ext>.gz\w{0,2})')
+
+    for root, dirs, files in os.walk(input_dir):
+        matching_files = fnmatch.filter(files, file_pattern)
+        for filename in matching_files:
+            m = naming_pattern.match(filename)
+            if m:
+                sample_info = { "files": [ os.path.abspath(os.path.join(root, filename)) ],
+                                "gzip" : m.group('gzip_ext')}
+                if m.groupdict()["read_number"]:
+                    # Read is one of a pair (paired-end data)
+                    read_2 = os.path.abspath("{0}_2.fastq{1}".format(
+                                     m.group('file_name'), m.group('gzip_ext')))
+                    #read_2 = files.pop(files.index(read_2_string))
+                    if os.path.exists(read_2):
+                        sample_info['files'].append(read_2)
+                    merge_file = "{0}.fastq".format(m.group('file_name'))
+                    sample_info["merge_file"] = "{0}.fastq".format(m.group('file_name'))
+                files_to_process.append(sample_info)
     if not files_to_process:
-        raise ValueError("No files matching pattern \"{0}\" found under directory \"{1}\". Exiting.".format(file_pattern, input_dir))
+        raise ValueError("No files matching pattern \"{0}\" found under directory" \
+                         " \"{1}\". Exiting.".format(file_pattern, input_dir))
     # Create the output directory if it does not exist
     if os.path.exists(output_dir):
         if not overwrite_output_dir:
-            raise OSError("Output directory \"{0}\" exists; will not overwrite.".format(output_dir))
+            raise OSError("Output directory \"{0}\" exists; " \
+            "will not overwrite.".format(output_dir))
     else:
         os.makedirs(output_dir)
     # Build the command
-    for pair in files_to_process:
+    for sample_set in files_to_process:
         cl =  ["process_radtags"]
         # -c : clean reads (remove reads with N)
         # -q : discard reads with low quality scores
@@ -72,32 +74,30 @@ def main(rest_enzyme_list = None, input_dir = os.getcwd(), file_pattern='*_1.fas
         cl += ["-e", rest_enzyme_list[0]]
         if len(rest_enzyme_list) > 1:
             cl += ["--renz_2", rest_enzyme_list[1]]
-        if pair["gzip"]:
+        if sample_set["gzip"]:
             cl += ["-i", "gzfastq"]
         cl += ["-o", output_dir]
-        if len(pair["files"]) == 2:
-            cl += ["-1", pair["files"][0], "-2", pair["files"][1]]
+        if len(sample_set["files"]) > 1:
+            cl += ["-1", sample_set["files"][0], "-2", sample_set["files"][1]]
         else:
-            cl += ["-f", pair["files"][0]]
+            cl += ["-f", sample_set["files"][0]]
         subprocess.check_call(cl)
-
-    # Merge individual reads into one file per sample
-        if paired_end and len(pair["files"]) > 1:
+        # Merge individual reads into one file per sample
+        if len(sample_set["files"]) > 1:
             files_to_merge = []
             merge_output_dir = os.path.join(output_dir, "merged")
             if not os.path.exists(merge_output_dir):
                 os.makedirs(merge_output_dir)
-            for file_to_merge in pair["files"]:
+            for file_to_merge in sample_set["files"]:
                 file_to_merge = os.path.basename(file_to_merge)
-                if pair["gzip"]:
+                if sample_set["gzip"]:
                     file_to_merge = os.path.splitext(file_to_merge)[0]
                 file_to_merge = os.path.join(output_dir, file_to_merge)
                 files_to_merge.append(file_to_merge)
-            with open(os.path.join(merge_output_dir, pair["merge_file"]), 'a+') as outf:
-                # TODO: print this to the log file
-                print("Merging files\n\t\"{0}\" and\n\t\"{1}\" into file\n\t\"{2}\"".format(files_to_merge[0], files_to_merge[1], outf.name), file=sys.stderr)
+            with open(os.path.join(merge_output_dir, sample_set["merge_file"]), 'a+') as outf:
+                print("Merging files\n\t\"{0}\" and\n\t\"{1}\" into file\n\t\"{2}\"".format(
+                      files_to_merge[0], files_to_merge[1], outf.name), file=sys.stderr)
                 for file in files_to_merge:
-                    #print "input file is {0}".format(file)
                     with open(file) as inf:
                         for line in inf:
                             outf.write(line)
@@ -105,9 +105,7 @@ def main(rest_enzyme_list = None, input_dir = os.getcwd(), file_pattern='*_1.fas
         columns = int(columns)
         if not columns:
             columns = 80
-        ## TODO: print this to the log file too
         print("-"*columns + "\n\n", file=sys.stderr)
-
         ## TODO, maybe: implement denovo_map.pl execution on all these files
 
 if __name__=="__main__":
@@ -121,8 +119,8 @@ if __name__=="__main__":
     parser.add_argument("-i", "--input-dir", default=os.getcwd(), help="The input directory to search for data. Default cwd.")
     parser.add_argument("-m", "--file-pattern", default="*_1.fastq*",
                         help="The pattern to use to locate files to be processed. Should correspond to read 1. Default \"*_1.fastq*\".")
-    parser.add_argument("-p", "--paired-end", action="store_true", dest="paired_end",
-                        help="Files are paired-end and should be merged after processing. Default false.")
+    #parser.add_argument("-p", "--paired-end", action="store_true", dest="paired_end",
+    #                    help="Files are paired-end and should be merged after processing. Default false.")
     parser.add_argument("-e", "--rest-enzyme", dest="rest_enzyme_list", action="append", required=True,
                         help="""The restriction enzyme used to digest the genomic DNA; can be used up to twice.
                                 Currently supported enzymes include:
@@ -131,8 +129,6 @@ if __name__=="__main__":
                                     'hindIII', 'mluCI', 'mseI', 'mspI', 'ndeI', 'nheI',
                                     'nlaIII', 'notI', 'nsiI', 'pstI', 'sau3AI', 'sbfI',
                                     'sexAI, 'sgrAI', 'sphI', 'taqI', or 'xbaI'.
-
-                                Default is ecoRI.
                                 """)
     ## TODO add --quiet option that just prints a progress bar
     args = vars(parser.parse_args())
